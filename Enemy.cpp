38f7298b78715e35927672ba1e59c42a9d8d6451
Arhum Z. Nayyar <47288337+ArhumZNayyar@users.noreply.github.com>
2021-07-16 17:52:24 -0400

    Added a new Enemy(JellyFish)

--- Enemy.cpp
+++ Enemy.cpp
@@ -78,8 +78,8 @@ Bat::Bat(Graphics &graphics, Vector2 spawnPoint) :
 	_startingX(spawnPoint.x),
 	_startingY(spawnPoint.y),
 	_shouldMoveUp(false),
-	_maxHealth(5),
-	_currentHealth(5)
+	_maxHealth(8),
+	_currentHealth(8)
 {
 	this->setupAnimations();
 	this->playAnimation("FlyLeft");
@@ -101,7 +101,7 @@ Bat::~Bat()
 }
 
 void Bat::update(int elapsedTime, Player &player) {
-	this->_fireBall.update();
+	this->_fireBall.updateFBall();
 	this->_HPBar.update();
 	this->_HPValue.update();
 	this->_direction = player.getX() > this->_x ? RIGHT : LEFT;
@@ -109,29 +109,35 @@ void Bat::update(int elapsedTime, Player &player) {
 		this->playAnimation(this->_direction == RIGHT ? "FlyRight" : "FlyLeft");
 		//Move up or down
 		this->_y += this->_shouldMoveUp ? -.06 : .06;
+		this->_x += this->_shouldMoveLeft ? -.016 : .016;
 		this->_HPBar._y += this->_shouldMoveUp ? -.06 : .06;
+		this->_HPBar._x += this->_shouldMoveLeft ? -.016 : .016;
 		this->_HPValue._y += this->_shouldMoveUp ? -.06 : .06;
+		this->_HPValue._x += this->_shouldMoveLeft ? -.016 : .016;
 		float hpNum = (float)this->getCurrentHealth() / this->getMaxHealth();
 		this->_HPValue.setSourceRectW(std::floor(hpNum * 17));
 
 		if (this->_y > (this->_startingY + 30) || this->_y < this->_startingY - 30) {
 			this->_shouldMoveUp = !this->_shouldMoveUp;
 		}
+		if (this->_x > (this->_startingX + 6) || this->_x < this->_startingX - 6) {
+			this->_shouldMoveLeft = !this->_shouldMoveLeft;
+		}
 		if (this->getBoundingBox().collidesWith(player.getBoundingBox())) {
-			player.gainHealth(-4.5);
+			player.gainHealth(-11.5);
 		}
 		if (_fireBall.getBoundingBox().collidesWith(player.getBoundingBox())) {
 			this->_fireBall.setX(this->_x);
 			this->_fireBall.setY(this->_y);
-			player.gainHealth(-8.5);
+			player.gainHealth(-15.5);
 		}
-		else if (_fireBall.getY() > this->_y + 160) {
+		else if (_fireBall.getY() > this->_y + 100 || _fireBall.getY() < this->_y - 100) {
 			this->_fireBall.setX(this->_x);
 			this->_fireBall.setY(this->_y);
 		}
 		else {
-			this->_fireBall.addX(player.getX() > this->_x ? .06 : -.06);
-			this->_fireBall.addY(player.getY() > this->_y ? .06 : -.06);
+			this->_fireBall.addX(player.getX() > this->_x ? .08 : -.08);
+			this->_fireBall.addY(player.getY() > this->_y ? .08 : -.08);
 		}
 	}
 	else if (this->getCurrentHealth() <= 0) {
@@ -164,7 +170,7 @@ void Bat::update(int elapsedTime, Player &player) {
 void Bat::draw(Graphics &graphics) {
 	if (isVisible == true) {
 		Enemy::draw(graphics);
-		this->_fireBall.draw(graphics, this->_fireBall.getX(), this->_fireBall.getY());
+		this->_fireBall.drawFBall(graphics, this->_fireBall.getX(), this->_fireBall.getY());
 		if (this->getCurrentHealth() < this->getMaxHealth()) {
 			this->_HPBar.draw(graphics, this->_HPBar._x, this->_HPBar._y);
 			this->_HPValue.draw(graphics, this->_HPValue._x, this->_HPValue._y);
@@ -202,7 +208,7 @@ void Bat::playAttack() {
 }
 
 void Bat::touchPlayer(Player* player) {
-		player->gainHealth(-1.0f);
+		player->gainHealth(-11.0f);
 }
 
 void Bat::bulletHit(float dmg) {
@@ -250,13 +256,13 @@ Shade::Shade(Graphics &graphics, Vector2 spawnPoint) :
 	_startingX(spawnPoint.x),
 	_startingY(spawnPoint.y),
 	_shouldMoveUp(false),
-	_currentHealth(100),
-	_maxHealth(100)
+	_currentHealth(110),
+	_maxHealth(110)
 {
 	this->setupAnimations();
 	this->playAnimation("shadeRight");
 
-	this->_shadeBall = Sprite(graphics, "data\\enemy\\shadeAttack.png", 0, 0, 22, 19, this->_startingX, (this->_startingY - 5));
+	this->_shadeBall = Sprite(graphics, "data\\enemy\\shadeAttack.png", 0, 0, 22, 19, this->_startingX, (this->_startingY + 5));
 	this->_HPBar = Sprite(graphics, "data\\enemy\\NpcCemet.png", 2, 157, 17, 7, this->_startingX + 56, this->_startingY - 15);
 	this->_HPValue = Sprite(graphics, "data\\enemy\\NpcCemet.png", 3, 174, 17, 5, this->_startingX + 57, this->_startingY - 12);
 	graphics.loadImage("data\\enemy\\shadeAttack.png");
@@ -275,8 +281,8 @@ void Shade::update(int elapsedTime, Player &player) {
 	this->_HPValue.update();
 	this->_direction = player.getX() > this->_x ? RIGHT : LEFT;
 	if (this->getCurrentHealth() > 0 && this->isVisible == true) {
-		if ((this->_direction == RIGHT && player.getX() > this->_x + 650) || (this->_direction == LEFT &&
-			player.getX() <= this->_x - 650)) {
+		if (this->isIdle == true && (this->_direction == RIGHT && player.getX() > this->_x + 350) || (this->_direction == LEFT &&
+			player.getX() <= this->_x - 350)) {
 			this->playAnimation("shadeIdle");
 			isIdle = true;
 		}
@@ -286,7 +292,22 @@ void Shade::update(int elapsedTime, Player &player) {
 
 			float hpNum = (float)this->getCurrentHealth() / this->getMaxHealth();
 			this->_HPValue.setSourceRectW(std::floor(hpNum * 17));
-
+			if (player.isGrounded() && player.getY() < this->_y + 15 && !this->_hasTeleported) {
+				this->_y = player.getY();
+				this->_x = player.getX();
+				this->_shadeBall._y = this->_y + 5;
+				this->_shadeBall._x = this->_x;
+				this->_HPBar._x = this->_x + 56;
+				this->_HPBar._y = this->_y - 15;
+				this->_HPValue._x = this->_x + 57;
+				this->_HPValue._y = this->_y - 12;
+				this->_hasTeleported = true;
+			}
+			if (this->_hasTeleported) {
+				this->_teleportTimer += elapsedTime;
+				if (this->_teleportTimer >= 1400)
+					this->_hasTeleported = false;
+			}
 			if (this->_direction == RIGHT) {
 				this->_x += 0.07f;
 				if (this->_boundingBox.collidesWith(player.getBoundingBox()))
@@ -306,19 +327,19 @@ void Shade::update(int elapsedTime, Player &player) {
 				this->_HPValue._y = this->_y - 62;
 			}
 			if (this->getBoundingBox().collidesWith(player.getBoundingBox())) {
-				player.gainHealth(-12.64f);
+				player.gainHealth(-18.64f);
 			}
 			if (_shadeBall.getBoundingBox().collidesWith(player.getBoundingBox())) {
 				this->_shadeBall.setX(this->_x);
-				this->_shadeBall.setY(this->_y - 5);
-				player.gainHealth(-26.69f);
+				this->_shadeBall.setY(this->_y + 5);
+				player.gainHealth(-38.69f);
 			}
-			else if (_shadeBall.getX() > this->_x + 350) {
+			else if (_shadeBall.getX() > this->_x + 290) {
 				this->_shadeBall.setX(this->_x);
 				this->_shadeBall.setY(this->_y + 5);
 			}
 			else {
-				this->_shadeBall.addX(player.getX() > this->_x ? .8 : -.8);
+				this->_shadeBall.addX(player.getX() > this->_x ? .55 : -.55);
 			}
 		}
 	}
@@ -443,6 +464,213 @@ float Shade::enemyExpAmount() {
 }
 
 std::string Shade::getName()
+{
+	return this->name;
+}
+
+
+//JellyFish class
+JellyFish::JellyFish() {}
+
+JellyFish::JellyFish(Graphics &graphics, Vector2 spawnPoint) :
+	Enemy(graphics, "data\\enemy\\NpcWeed.png", 273, 0, 45, 24, spawnPoint, 140),
+	_startingX(spawnPoint.x),
+	_startingY(spawnPoint.y),
+	_shouldMoveUp(false),
+	_maxHealth(10),
+	_currentHealth(10)
+{
+	this->setupAnimations();
+	this->playAnimation("JellyLeft");
+	this->_fireX = this->_startingX;
+	this->_fireY = this->_startingY;
+	this->_HPBar._x = this->_startingX;
+	this->_HPBar._y = this->_startingY;
+	this->_HPBar = Sprite(graphics, "data\\enemy\\NpcCemet.png", 2, 157, 17, 7, this->_HPBar._x + 25, this->_HPBar._y - 15);
+	this->_HPValue = Sprite(graphics, "data\\enemy\\NpcCemet.png", 3, 167, 17, 5, this->_HPBar._x + 1, this->_HPBar._y + 2);
+}
+
+JellyFish::~JellyFish()
+{
+	this->destroySprite();
+	this->_HPBar.destroySprite();
+	this->_HPValue.destroySprite();
+}
+
+void JellyFish::update(int elapsedTime, Player &player) {
+	this->_y += this->_dy * elapsedTime; //Gravity move them by Y
+	this->_HPBar.update();
+	this->_HPValue.update();
+	this->_direction = player.getX() > this->_x ? RIGHT : LEFT;
+	if (this->getCurrentHealth() > 0 && this->isVisible == true) {
+		this->playAnimation(this->_direction == RIGHT ? "JellyRight" : "JellyLeft");
+		//Move up or down
+		if (this->_JellyAttack == false && this->_resettingPos == false && this->_delayAttack == false) {
+			this->_y += this->_shouldMoveUp ? -.06 : .06;
+			this->_HPBar._y += this->_shouldMoveUp ? -.06 : .06;
+			this->_HPValue._y += this->_shouldMoveUp ? -.06 : .06;
+			if (this->_y > (this->_startingY + 30) || this->_y < this->_startingY - 30) {
+				this->_shouldMoveUp = !this->_shouldMoveUp;
+			}
+		}
+		if (this->_delayAttack == false && this->_resettingPos == false && this->_y - 15 < player.getY() && 
+			(int)this->_x + 20 == (int)player.getX()) {
+				this->_JellyAttack = true;
+		}
+		if (this->_resettingPos == true && this->_delayAttack == false) {
+			this->_y -= 0.18;
+			if (this->_y <= this->_startingY) {
+				std::cout << "reset pos...y <= starting y" << std::endl;
+				this->_resettingPos = false;
+				this->_delayAttack = true;
+			}
+		}
+		if (this->_delayAttack == true && this->_resettingPos == false) {
+			this->_delayTimer += elapsedTime;
+			if (this->_delayTimer >= 88) {
+				std::cout << "Delay over. " << std::endl;
+				this->_delayAttack = false;
+				this->_delayTimer = 0;
+			}
+		}
+		if (this->_JellyAttack == true) {
+			this->_y += 1.25;
+		}
+
+
+		float hpNum = (float)this->getCurrentHealth() / this->getMaxHealth();
+		this->_HPValue.setSourceRectW(std::floor(hpNum * 17));
+		if (this->getBoundingBox().collidesWith(player.getBoundingBox())) {
+			player.gainHealth(-35.5);
+		}
+	}
+	else if (this->getCurrentHealth() <= 0) {
+		_deathTimeElapsed += elapsedTime;
+		if (isVisible == false)
+			timerRespawn += elapsedTime;
+		if (_deathTimeElapsed > _deathAnimationTime && isVisible == true && dyingAnimation == true) {
+			this->dyingAnimation = false;
+			this->_deathTimeElapsed = 0;
+			this->isVisible = false;
+		}
+		else if (_deathTimeElapsed < _deathAnimationTime && isVisible == true) {
+			this->dyingAnimation = true;
+			this->playAnimation("JellyFishDie");
+		}
+		if (timerRespawn > respawnTime && isVisible == false && dyingAnimation == false) {
+			timerRespawn = 0;
+			this->isVisible = true;
+			this->_currentHealth = this->_maxHealth;
+			this->removeEnemy = false;
+			this->canDropLoot = false;
+			_deathTimeElapsed = 0;
+		}
+	}
+
+	Enemy::update(elapsedTime, player);
+}
+
+
+void JellyFish::draw(Graphics &graphics) {
+	if (isVisible == true) {
+		Enemy::draw(graphics);
+		if (this->getCurrentHealth() < this->getMaxHealth()) {
+			this->_HPBar.draw(graphics, this->_HPBar._x, this->_HPBar._y);
+			this->_HPValue.draw(graphics, this->_HPValue._x, this->_HPValue._y);
+		}
+	}
+}
+
+void JellyFish::animationDone(std::string currentAnimation) {
+	if (this->getCurrentHealth() <= 0) {
+		if (this->canDropLoot == false) {
+			this->removeEnemy = true;
+			this->canDropLoot = true;
+		}
+	}
+}
+
+void JellyFish::dropLoot(Player &player) {
+
+}
+
+void JellyFish::setupAnimations() {
+	this->addSpecialAnimation(1, 228, 0, "JellyLeft", 45, 24, Vector2(0, 0));
+	this->addSpecialAnimation(1, 228, 24, "JellyRight", 45, 24, Vector2(0, 0));
+	this->addAnimation(7, 0, 270, "JellyFishDie", 16, 16, Vector2(0, 0));
+}
+
+void JellyFish::playDeath() {
+	this->playAnimation("JellyFishDie", true);
+}
+
+void JellyFish::playAttack() {
+
+}
+
+void JellyFish::touchPlayer(Player* player) {
+	player->gainHealth(-1.0f);
+}
+
+void JellyFish::bulletHit(float dmg) {
+	if (this->_currentHealth >= 1)
+		this->_currentHealth -= dmg;
+}
+
+void JellyFish::setRemoveable() {
+	if (removeEnemy == true) {
+		removeEnemy = false;
+	}
+}
+
+void JellyFish::handleEnemyTileCollision(std::vector<Rectangle>& others)
+{
+	for (int i = 0; i < others.size(); i++) {
+		sides::Side collisionSide = Sprite::getCollisionSide(others.at(i));
+		if (collisionSide != sides::NONE) {
+			switch (collisionSide) {
+			case sides::TOP:
+				this->_dy = 0; //reset all gravity, if we arent grounded we fall to the ground
+				//this->_y = others.at(i).getBottom() + 1; //no longer go through things, stops us
+				//this->_JellyAttack = false;
+				//this->_resettingPos = true;
+				//this->_HPBar._y = others.at(i).getTop() + 1;
+				//this->_HPValue._y = others.at(i).getTop() + 1;
+				//if (this->_grounded) { //only time we hit a top tile is if we are on a slope, (we are grounded on a slope)
+				//	//this->_dx = 0; //stop movement on x-axis
+				//	this->_x -= this->_direction == RIGHT ? 0.5f : -0.5f; //if we face right, subtract .5 from x pos otherwise subtract -.5 (adds .5)
+				//}
+				break;
+			case sides::BOTTOM: //hit the top (bottom) of tile push us back up ontop of tile
+				//this->_y = others.at(i).getTop() - this->_boundingBox.getHeight() - 1;
+				this->_y -= 10;
+				//this->_HPBar._y = others.at(i).getTop() - this->_boundingBox.getHeight() - 1;
+				//this->_HPValue._y = others.at(i).getTop() - this->_boundingBox.getHeight() - 1;
+				this->_dy = 0;
+				//this->_grounded = true; //we are on ground since it pushed it back up
+				this->_JellyAttack = false;
+				this->_resettingPos = true;
+				break;
+			case sides::LEFT:
+				this->_x = others.at(i).getRight() + 1;
+				break;
+			case sides::RIGHT:
+				this->_x = others.at(i).getLeft() - this->_boundingBox.getWidth() - 1;
+				break;
+			}
+		}
+	}
+}
+
+bool JellyFish::isRemoveable() {
+	return removeEnemy;
+}
+
+float JellyFish::enemyExpAmount() {
+	return JellyFish::JellyFishExp;
+}
+
+std::string JellyFish::getName()
 {
 	return this->name;
 }
--- Enemy.h
+++ Enemy.h
@@ -110,6 +110,7 @@ public:
 private:
 	float _startingX, _startingY;
 	bool _shouldMoveUp; //keep track of if bat should move
+	bool _shouldMoveLeft;
 	bool hasHit = false;
 	int iFrameCount = 0;
 	float batExp = 1.175f;
@@ -124,7 +125,7 @@ protected:
 
 	int _maxHealth;
 	int _currentHealth;
-	int level = 1;
+	int level = 0;
 	std::string name = "bat";
 	std::string coinType = "bronze";
 	bool boss = false;
@@ -186,11 +187,12 @@ private:
 	//float _fireX, _fireY;
 	double _deathAnimationTime = 800;
 	double _deathTimeElapsed = 0;
+	float _teleportTimer = 0;
 protected:
 	Direction _direction;
 	int _maxHealth;
 	int _currentHealth;
-	int level = 10;
+	int level = 4;
 	std::string name = "shade";
 	std::string coinType = "red";
 	bool boss = true;
@@ -199,6 +201,7 @@ protected:
 	bool canDropLoot = false;
 	bool dyingAnimation = true;
 	bool _grounded = true;
+	bool _hasTeleported = false;
 	float _dy = 0;
 	const float GRAVITY = 0.002f;
 	const float GRAVITY_CAP = 0.8f;
@@ -206,4 +209,82 @@ protected:
 	double timerRespawn = 0;
 	bool isVisible = true;
 	bool isIdle = true;
+};
+
+class JellyFish : public Enemy {
+public:
+	JellyFish();
+	JellyFish(Graphics &graphics, Vector2 spawPoint);
+	~JellyFish();
+	JellyFish& operator=(const JellyFish& levelMap);
+	JellyFish(const JellyFish&);
+
+	void update(int elapsedTime, Player &player);
+	void draw(Graphics &graphics);
+	void touchPlayer(Player* player);
+	void playDeath();
+	void animationDone(std::string currentAnimation);
+	void setupAnimations();
+	void dropLoot(Player &player);
+	void playAttack();
+
+	void bulletHit(float dmg);
+	const inline int getMaxHealth() const { return this->_maxHealth; }
+	const inline int getCurrentHealth() const { return this->_currentHealth; }
+	const inline int getEnemyLevel() const { return this->level; }
+	const inline bool getDyingState() const { return this->dyingAnimation; }
+	void setDyingState(bool condition) { this->dyingAnimation = condition; }
+	inline void setVisible(bool condition) { this->isVisible = condition; }
+
+	const inline std::string getCoinDropType() { return this->coinType; }
+	const inline bool isBoss() { return this->boss; }
+	const inline bool isMiniBoss() { return this->miniBoss; }
+	const inline float getStartingX() const { return this->_startingX; }
+	const inline float getStartingY() const { return this->_startingY; }
+
+	void handleEnemyTileCollision(std::vector<Rectangle> &others);
+
+	bool isRemoveable();
+	void setRemoveable();
+
+	bool removeEnemy = false;
+
+	float enemyExpAmount();
+
+	std::string getName();
+private:
+	float _startingX, _startingY;
+	bool _shouldMoveUp; //keep track of if bat should move
+	bool hasHit = false;
+	int iFrameCount = 0;
+	float JellyFishExp = 1.175f;
+	Sprite _fireBall;
+	Sprite _HPBar;
+	Sprite _HPValue;
+	float _fireX, _fireY;
+	bool _JellyAttack = false;
+	bool _resettingPos = false;
+	bool _delayAttack = false;
+	float _delayTimer = 0;
+	double _deathAnimationTime = 800;
+	double _deathTimeElapsed = 0;
+protected:
+	Direction _direction;
+	bool _grounded = true;
+	float _dy = 0;
+	const float GRAVITY = 0.002f;
+	const float GRAVITY_CAP = 0.8f;
+	int _maxHealth;
+	int _currentHealth;
+	int level = 3;
+	std::string name = "JellyFish";
+	std::string coinType = "bronze";
+	bool boss = false;
+	bool miniBoss = false;
+	bool deathCreated = false;
+	bool canDropLoot = false;
+	bool dyingAnimation = true;
+	double respawnTime = 6000;
+	double timerRespawn = 0;
+	bool isVisible = true;
 };
