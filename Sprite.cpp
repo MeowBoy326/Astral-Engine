bbb52cfa70bcf93301ccf497c0754fbc94c4699d
Arhum Z. Nayyar <47288337+ArhumZNayyar@users.noreply.github.com>
2022-01-09 03:05:42 -0500

    Corrected the bounding box for Bosses
    Bosses will now have the correct bounding box that matches their sprite's animation position.

--- Sprite.cpp
+++ Sprite.cpp
@@ -189,7 +189,7 @@ void Sprite::update() {
 }
 
 void Sprite::updateBoss(float y) {
-	this->_boundingBox = Rectangle(this->_x, y, this->_sourceRect.w * globals::SPRITE_SCALE, this->_sourceRect.h * globals::SPRITE_SCALE);
+	this->_boundingBox = Rectangle(this->_x, this->_y - (this->_sourceRect.h * globals::SPRITE_SCALE), this->_sourceRect.w * globals::SPRITE_SCALE * 2, this->_sourceRect.h * globals::SPRITE_SCALE * 2);
 }
 
 void Sprite::updateFBall()
