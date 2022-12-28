3fe32f45dd04cea834ef6ab63f6c86f1e8a0be46
Arhum Z. Nayyar <47288337+ArhumZNayyar@users.noreply.github.com>
2022-12-28 18:26:43 -0500

    Update questTable parameters

--- main/headers/Npc.h
+++ main/headers/Npc.h
@@ -66,11 +66,11 @@ public:
 protected:
 	Direction _direction;
 	// QuestName, type, object, amount, dialogueText(Vector of strings), finishText(Vector of strings),
-	// npcID (formerly npcName), rewardItemID, rewardAmount, exp, cels
+	// npcID (formerly npcName), rewardItemID, rewardAmount, exp, cels, isCompleted
 	std::vector<std::tuple<std::string, int, std::string, int, std::vector<std::string>, std::vector<std::string>,
-	int, int, int, int, int>> questTable;
-	// QuestName, type, object, amount, isCompleted, isRewarded
-	std::vector<std::tuple<std::string, int, std::string, int, bool, bool, int, std::string, int, int>> questLog;
+	int, int, int, int, int, bool>> questTable;
+	// QuestName, type, object, amount, isCompleted, isRewarded, 
+	//std::vector<std::tuple<std::string, int, std::string, int, bool, bool, int, std::string, int, int>> questLog;
 	// ID of NPC, DialogueID player has reached
 	std::vector<std::pair<int, int>> npcDialogueTable;
 	// NPC ID table - Stores the name with the corresponding ID
