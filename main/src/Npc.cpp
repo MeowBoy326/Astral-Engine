351272c9d76295e69cda8b876a65fb71e29fd60f
Arhum Z. Nayyar <47288337+ArhumZNayyar@users.noreply.github.com>
2022-12-29 16:18:52 -0500

    Remove unused NPC code

--- main/src/Npc.cpp
+++ main/src/Npc.cpp
@@ -80,37 +80,6 @@ void Npc::drawNpcIcon(Graphics & graphics, const std::string name, int posX, int
 int Npc::playScript(int npcID, Graphics & graphics, int posX, int posY)
 {
 	this->endOfChat = false;
-	//XMLDocument xml;
-	//XMLError result;
-	//std::filesystem::path cwd = std::filesystem::current_path() / "data" / "npc";
-	//cwd.append(name + ".xml");
-	//std::filesystem::path nCwd = std::filesystem::current_path() / "data" / "npc";
-	//nCwd.append("temp.xml");
-	//AESCipher cipher;
-	//cipher.AESDecrypt(cwd.string(), nCwd.string());
-	//result = xml.LoadFile(nCwd.string().c_str());
-	//if (result != tinyxml2::XML_SUCCESS)
-	//	std::cout << "Could not read the file!";
-	//XMLNode* root = xml.FirstChild();
-	//if (root == nullptr) {
-	//	std::cout << XML_ERROR_FILE_READ_ERROR << std::endl;
-	//	return XML_ERROR_FILE_READ_ERROR;
-	//}
-	//XMLElement* element = root->FirstChildElement("Talk");
-	//if (element == nullptr)
-	//	return XML_ERROR_PARSING_ELEMENT;
-	//// Get number of lines here
-	//result = element->QueryIntAttribute("lines", &this->lines);
-	//const char* textPtr = nullptr;
-	//textPtr = element->Attribute((char*)&this->lineChar);
-	//std::string text = textPtr;
-	////this->drawNpcText(graphics, 100, 100, text, posX, posY);
-	//this->drawNpcDialogue(graphics, 100, 100, text, posX, posY);
-	//cipher.AESEncrypt(nCwd.string(), cwd.string());
-	//XMLCheckResult(result);
-
-	//if (this->npcDialogueTable)
-
 
 	lua_State* L = luaL_newstate(); // create a new Lua state
 	luaL_openlibs(L); // load Lua standard libraries
@@ -160,43 +129,6 @@ int Npc::playScript(int npcID, Graphics & graphics, int posX, int posY)
 
 int Npc::playNext(int npcID, Graphics & graphics, int posX, int posY, Player &player)
 {
-	// If (questState == false) {
-		/*XMLDocument xml;
-		std::filesystem::path cwd = std::filesystem::current_path() / "data" / "npc";
-		cwd.append(name + ".xml");
-		std::filesystem::path nCwd = std::filesystem::current_path() / "data" / "npc";
-		nCwd.append("temp.xml");
-		AESCipher cipher;
-		cipher.AESDecrypt(cwd.string(), nCwd.string());
-		xml.LoadFile(nCwd.string().c_str());
-		XMLError result;
-		XMLNode* root = xml.FirstChild();
-		if (root == nullptr)
-			return XML_ERROR_FILE_READ_ERROR;
-		XMLElement* element = root->FirstChildElement("Talk");
-		if (element == nullptr)
-			return XML_ERROR_PARSING_ELEMENT;
-		result = element->QueryIntAttribute("lines", &this->lines);
-		const char* textPtr = nullptr;
-		this->lineChar++;
-		this->lineCounter++;
-		if (this->lineCounter <= this->lines) {
-			textPtr = element->Attribute((char*)&this->lineChar);
-			std::string text;
-			text = textPtr;
-			this->drawNpcText(graphics, 100, 100, text, posX, posY);
-			cipher.AESEncrypt(nCwd.string(), cwd.string());
-			XMLCheckResult(result);
-		}
-		else {
-			this->lineCounter = 1;
-			this->lineChar = 'a';
-			this->endOfChat = true;
-			cipher.AESEncrypt(nCwd.string(), cwd.string());
-			XMLCheckResult(result);
-		}
-		XMLCheckResult(result);*/
-
 	int lineID;
 
 	auto it = std::find_if(npcDialogueTable.begin(), npcDialogueTable.end(), [&npcID](const auto& t) {return t.first == npcID; });
@@ -253,7 +185,6 @@ int Npc::playQuest(int npcID, int selection, Graphics& graphics, int posX, int p
 	this->endOfChat = false;
 	this->isQuestDone = false;
 	this->isQuestRewarded = false;
-	
 
 	if (selection == 1)
 		this->storedQuestName = this->questName1;
@@ -275,12 +206,10 @@ int Npc::playQuest(int npcID, int selection, Graphics& graphics, int posX, int p
 					for (int i = 0; i < this->questDialogueSize; i++) {
 						this->currentQuestDialogue.push_back(std::get<5>(t)[i]);
 					}
-					//this->questDialogueText = std::get<4>(t)[0];
 				}
 			}
 		}
 	}
-
 	else {
 		// Retrieve the text and store into a vector that serves only the currently selected quest
 		// As many quests will be in the game, searching through all the quests each time can become expensive
@@ -291,7 +220,6 @@ int Npc::playQuest(int npcID, int selection, Graphics& graphics, int posX, int p
 					for (int i = 0; i < this->questDialogueSize; i++) {
 						this->currentQuestDialogue.push_back(std::get<4>(t)[i]);
 					}
-					//this->questDialogueText = std::get<4>(t)[0];
 				}
 			}
 		}
@@ -299,49 +227,6 @@ int Npc::playQuest(int npcID, int selection, Graphics& graphics, int posX, int p
 
 	// Start at line 0
 	this->questDialogueLine = 0;
-
-	//lua_State* L = luaL_newstate(); // create a new Lua state
-	//luaL_openlibs(L); // load Lua standard libraries
-	//std::string scriptName = "data/npc/" + std::to_string(npcID) + ".lua";
-	//std::cout << scriptName << std::endl;
-	//luaL_dofile(L, scriptName.c_str()); // load the dialogue script file
-
-	//luabridge::LuaRef questsTable = luabridge::getGlobal(L, "quests");
-	//luabridge::LuaRef quest = questsTable;
-	//luabridge::LuaRef dialogueTable = quest["qDialogue"];
-
-	//int lineID, lines;
-	//lines = dialogueTable.length();
-
-
-	//auto it = std::find_if(npcDialogueTable.begin(), npcDialogueTable.end(), [&npcID](const auto& t) {return t.first == npcID; });
-	//auto distance = std::distance(this->npcDialogueTable.begin(), it);
-	//if (it != npcDialogueTable.end()) {
-	//	if (npcDialogueTable[distance].second > lines) {
-	//		// Set to +1 of the max amount of lines so we don't have to deal with lineID becoming a large number.
-	//		lineID = lines + 1;
-	//		npcDialogueTable[distance].second = lineID;
-	//	}
-	//	lineID = npcDialogueTable[distance].second;
-	//}
-	//else {
-	//	this->npcDialogueTable.push_back(std::make_pair(npcID, 1));
-	//	lineID = 1;
-	//}
-
-	//// push the function and arguments onto the stack
-	//lua_getglobal(L, "getDialogue"); // push the function
-	//lua_pushinteger(L, lineID); // push the NPC ID argument
-
-	//// call the function with 1 argument and 1 result
-	//lua_call(L, 1, 1);
-
-	//// retrieve the result from the top of the stack
-	//this->dialogueText = lua_tostring(L, -1);
-
-	//// clean up the stack and close the Lua state
-	//lua_pop(L, 1);
-	//lua_close(L);
 
 	this->drawNpcDialogue(graphics, 100, 100, this->currentQuestDialogue[this->questDialogueLine], posX, posY);
 	return 0;
@@ -372,9 +257,6 @@ int Npc::repeatQuestScript(Graphics& graphics, int posX, int posY) {
 
 void Npc::resetScripts()
 {
-	this->lines = 1;
-	this->lineCounter = 1;
-	this->lineChar = 'a';
 	this->endOfChat = false;
 }
 
@@ -480,19 +362,6 @@ int Npc::loadQuests(int npcID) {
 		lua_close(L);
 	}
 
-	// Once rewarded and player opens menu again, mark as completed.
-	//for (auto &t : this->questLog) {
-	//	if (std::get<5>(t) == true && std::get<4>(t) == false) {
-	//		std::get<4>(t) = true;
-
-	//	}
-	//}
-
-
-	//std::cout << "loading quests for: " << name << std::endl;
-	//XMLDocument xml;
-	//XMLError result;
-
 	//std::filesystem::path cwd = std::filesystem::current_path() / "data" / "npc";
 	//cwd.append(name + ".xml");
 	//std::filesystem::path nCwd = std::filesystem::current_path() / "data" / "npc";
@@ -500,40 +369,8 @@ int Npc::loadQuests(int npcID) {
 	//AESCipher cipher;
 	//cipher.AESDecrypt(cwd.string(), nCwd.string());
 	//result = xml.LoadFile(nCwd.string().c_str());
-
-	//if (result != tinyxml2::XML_SUCCESS)
-	//	std::cout << "Could not read the file!";
-	//XMLNode* root = xml.FirstChild();
-	//if (root == nullptr) {
-	//	std::cout << XML_ERROR_FILE_READ_ERROR << std::endl;
-	//	return XML_ERROR_FILE_READ_ERROR;
-	//}
-	//XMLElement* element = root->FirstChildElement("QuestTable");
-	//XMLElement* ptrElement = element->FirstChildElement("Quest");
-	//if (ptrElement == nullptr)
-	//	return XML_ERROR_PARSING_ELEMENT;
-	//int amount, type, numOfQuest, rewardType, rewardLevel, exp, cels;
-	//ptrElement->QueryIntAttribute("numOfQuest", &numOfQuest);
-	//const char* textPtr = nullptr, *descPtr = nullptr, *objPtr = nullptr, *npcPtr = nullptr, *rewardItem;
-	//for (int counter = 0; counter < numOfQuest; ++counter) {
-	//	// Obtain data from XML
-	//	ptrElement->QueryIntAttribute("amount", &amount);
-	//	ptrElement->QueryIntAttribute("type", &type);
-	//	textPtr = ptrElement->Attribute("questName");
-	//	descPtr = ptrElement->Attribute("description");
-	//	objPtr = ptrElement->Attribute("object");
-	//	ptrElement->QueryIntAttribute("rewardType", &rewardType);
-	//	// If (rewardType == 0) {
-	//	// PtrElement->QueryIntAttribute("reward", &rewardLevel);
-	//	//}
-	//	// Else
-	//	rewardItem = ptrElement->Attribute("reward");
-	//	ptrElement->QueryIntAttribute("exp", &exp);
-	//	ptrElement->QueryIntAttribute("cels", &cels);
-	//	std::string text = textPtr, descText = descPtr, objText = objPtr;
-
 	//cipher.AESEncrypt(nCwd.string(), cwd.string());
-	//XMLCheckResult(result);
+
 	return 0;
 }
 
@@ -575,59 +412,6 @@ void Npc::questSelection(Graphics & graphics, int posX, int posY, int selection)
 	}
 }
 
-//void Npc::acceptQuest(Graphics & graphics, int npcID, int posX, int posY, Player & player, int selection)
-//{
-//	this->questState = true;
-//	if (questDone) {
-//		this->giveRewards(graphics, this->storedQuestName, posX, posY, player, selection);
-//		return;
-//	}
-//	bool isCompleted;
-//	if (questTable.empty())
-//		return;
-//	if (questName1 || questName2)
-//	if (selection >= this->questTable.size()) { // If player tries to select and empty quest index
-//		std::cout << "Cannot find specified index..." << std::endl;
-//		return;
-//	}	
-//	//std::string text = std::get<0>(this->questTable[selection]);
-//	//this->storedQuestName = text;
-//
-//
-//
-//
-//	if (!questLog.empty() && !questTable.empty()) {
-//		isCompleted = this->checkQuest(graphics, text, posX, posY, player);
-//		if (isCompleted == true) {
-//			this->questDone = true;
-//			this->giveRewards(graphics, this->storedQuestName, posX, posY, player, selection);
-//			return;
-//		}
-//	}
-//	std::string completeMsg;
-//	auto it = std::find_if(questLog.begin(), questLog.end(), [&text](const auto& t) {return std::get<0>(t) == text; });
-//	auto tableIt = std::find_if(questTable.begin(), questTable.end(), [&text](const auto& t) {return std::get<0>(t) == text; });
-//	auto distance = std::distance(this->questLog.begin(), it);
-//	 if (!questTable.empty()) {
-//		this->drawNpcText(graphics, 100, 100, std::get<4>(this->questTable[selection]), posX, posY);
-//		if (this->questLog.empty()) {
-//			this->questLog.push_back(std::make_tuple(std::get<0>(this->questTable[selection]), std::get<1>(this->questTable[selection]),
-//				std::get<2>(this->questTable[selection]), std::get<3>(this->questTable[selection]), false, false, 
-//				std::get<6>(this->questTable[selection]), std::get<7>(this->questTable[selection]), 
-//				std::get<8>(this->questTable[selection]), std::get<9>(this->questTable[selection])));
-//		}
-//		else { // Find_if algorithm with lambda function
-//			auto it = std::find_if(questLog.begin(), questLog.end(), [&text](const auto& t) {return std::get<0>(t) == text; });
-//			if (it == questLog.end()) {
-//				this->questLog.push_back(std::make_tuple(std::get<0>(this->questTable[selection]), std::get<1>(this->questTable[selection]),
-//					std::get<2>(this->questTable[selection]), std::get<3>(this->questTable[selection]), false, false,
-//					std::get<6>(this->questTable[selection]), std::get<7>(this->questTable[selection]),
-//					std::get<8>(this->questTable[selection]), std::get<9>(this->questTable[selection])));
-//			}
-//		}
-//	}
-//}
-
 void Npc::giveRewards(Player & player)
 {
 	if (!this->questTable.empty()) {
@@ -656,56 +440,6 @@ void Npc::giveRewards(Player & player)
 			}
 		}
 	}
-
-
-	//std::string completeMsg;
-	//std::stringstream ss;
-	//auto it = std::find_if(questLog.begin(), questLog.end(), [&questName](const auto& t) {return std::get<0>(t) == questName; });
-	//auto distance = std::distance(this->questLog.begin(), it);
-	//// Auto itQT = std::find_if(questTable.begin(), questTable.end(), [&npcName](const auto& t) {return std::get<0>(t) == npcName; });
-	//// If (itQT == questTable.end()) {
-	////	std::cout << "COULD NOT FIND QUEST IN QUESTABLE!!!" << std::endl;
-	////}
-	//// Auto distanceQT = std::distance(questTable.begin(), itQT);
-	//if (questDone) {
-	//	if (this->exp >= 0) {
-	//		if (std::get<6>(this->questLog[distance]) == 0) {
-	//			ss << "Here is your reward!$n" << this->levelReward << " level," << this->exp << "exp," << this->cels << "cels.$e";
-	//			this->drawNpcText(graphics, 100, 100, ss.str(), posX, posY);
-	//		}
-	//		else if (std::get<6>(this->questLog[distance]) == 1) {
-	//			ss << "Here is your reward!$n" << this->reward << " item," << this->exp << "exp," << this->cels << "cels.$e";
-	//			this->drawNpcText(graphics, 100, 100, ss.str(), posX, posY);
-	//		}
-	//	}
-	//	if (std::get<5>(this->questLog[distance]) != true) {
-	//		if (std::get<6>(this->questLog[distance]) == 0) {
-	//			std::cout << "rewardType = " << std::get<6>(this->questLog[distance]) << std::endl;
-	//			this->levelReward = std::stoi(std::get<7>(this->questLog[distance]));
-	//			this->exp = std::get<8>(this->questLog[distance]);
-	//			this->cels = std::get<9>(this->questLog[distance]);
-	//			player.addLevel(levelReward);
-	//			player.gainExp(exp);
-	//			player.gainCurrency(cels);
-	//			ss << "Here is your reward!$n" << levelReward << " level," << exp << "exp," << cels << "cels.$e";
-	//			// CompleteMsg = "Thank you! Here is your reward.$n" + levelGain + "level, "; // TODO: push rewards into vector to distibute any reward for any quest.
-	//			this->drawNpcText(graphics, 100, 100, ss.str(), posX, posY);
-	//		}
-	//		else if (std::get<6>(this->questLog[distance]) == 1) {
-	//			std::cout << "rewardType = " << std::get<6>(this->questLog[distance]) << std::endl;
-	//			this->exp = std::get<8>(this->questLog[distance]);
-	//			this->cels = std::get<9>(this->questLog[distance]);
-	//			this->reward = std::get<7>(this->questLog[distance]);
-	//			player.addEquipment(reward);
-	//			player.gainExp(exp);
-	//			player.gainCurrency(cels);
-	//			ss << "Here is your reward!$n" << reward << " item," << exp << "exp," << cels << "cels.$e";
-	//			this->drawNpcText(graphics, 100, 100, ss.str(), posX, posY);
-	//		}
-	//	}
-	//		
-	//	std::get<5>(this->questLog[distance]) = true;
-	//}
 }
 
 
@@ -730,26 +464,6 @@ bool Npc::checkQuest(Player &player)
 			this->blockSelectedQuest = true;
 		}
 	}
-
-	//if (!this->questLog.empty()) {
-	//	std::string text = name;
-	//	std::string completeMsg;
-	//	auto it = std::find_if(questLog.begin(), questLog.end(), [&text](const auto& t) {return std::get<0>(t) == text; });
-	//	auto tableIt = std::find_if(questTable.begin(), questTable.end(), [&text](const auto& t) {return std::get<0>(t) == text; });
-	//	auto distanceTable = std::distance(this->questTable.begin(), tableIt);
-	//	auto distance = std::distance(this->questLog.begin(), it);
-	//	if (it != questLog.end() && std::get<4>(this->questLog[distance]) == false) {
-	//		if (std::get<1>(this->questLog[distance]) == 1) { // Kill quests
-	//			if (player.checkKillQuestComplete(std::get<2>(this->questLog[distance]), std::get<3>(this->questLog[distance]))) {
-	//				std::cout << "Quest is completed! Remove from available quests!" << std::endl;
-	//				if (tableIt != questTable.end()) {
-	//					this->questTable.erase(this->questTable.begin() + distanceTable);
-	//				}
-	//				return true;
-	//			}
-	//		}
-	//	}
-	//}
 	return false;
 }
 
