470b3d30a9e4c255740c8035c1f84021f1dce9ab
Arhum Z. Nayyar <47288337+ArhumZNayyar@users.noreply.github.com>
2022-12-30 16:52:08 -0500

    Refactor Items class
    Items class does not extend off the inventory and uses virtual functions to define different types of items in the game.  Allowing for each item to have it's own use() function that will perform special interactions.

--- main/headers/Items.h
+++ main/headers/Items.h
@@ -4,30 +4,39 @@
 #include <vector>
 #include "AnimatedSprite.h"
 #include "Player.h"
-#include "Inventory.h"
-
+#include <iostream>
 
 class Graphics;
 
-
-class Items : public AnimatedSprite, public Inventory
+class Items : public AnimatedSprite
 {
 public:
 	Items();
 	Items(Graphics &graphics);
+
+	typedef int ItemID;
+	ItemID getID() const { return _id; }
+	bool isCurrency() const { return currencyItem; }
+	virtual Items* clone() const = 0;
+	virtual void use(Player &player) = 0;
+	virtual ~Items() {};
+
+	/* old functions */
 	virtual void update(int elapsedTime, Player & player);
 	virtual void draw(Graphics & graphics);
 	virtual void drawDrops(Graphics &graphics, float x, float y);
-	void addToInventory(int type);
+	//void addToInventory(int type);
 	virtual const inline bool isDroppedItem() { return this->wasDropped; }
 	virtual const inline int getAmount() { return this->currencyAmount; }
 	Items(Graphics &graphics, std::string filePath, int sourceX, int sourceY, int width, int height, Vector2 spawnPoint, int timeToUpdate);
-	~Items();
 private:
 	std::vector<std::pair<std::string, int>> lootTable;
 	std::vector<std::pair<Items*, std::string>> mobDrops; 
 	bool wasDropped;
 	int currencyAmount;
+protected:
+	ItemID _id;
+	bool currencyItem;
 };
 
 class HealthPotion : public Items {
@@ -36,7 +45,18 @@ public:
 	HealthPotion(Graphics &graphics, Vector2 spawnPoint);
 	~HealthPotion();
 
-	void addToInventory();
+	static const ItemID ID = 0;
+
+	void use(Player &player) override {
+		// Implement HP pot
+		std::cout << "NEW HEAL USED!" << std::endl;
+		player.gainHealth(player.getMaxHealth() / 3);
+	}
+
+	Items* clone() const override { return new HealthPotion(*this); }
+
+
+	//void addToInventory();
 	const inline bool isDroppedItem() { return this->wasDropped; }
 	const inline int getAmount() { return this->currencyAmount; }
 	void update(int elapsedTime, Player & player);
@@ -57,6 +77,14 @@ public:
 	PermHP();
 	PermHP(Graphics &graphics, Vector2 spawnPoint);
 	~PermHP();
+
+	static const ItemID ID = 1;
+
+	void use(Player &player) override {}
+
+	Items* clone() const override { return new PermHP(*this); }
+
+
 	const inline bool isDroppedItem() { return this->wasDropped; }
 	const inline int getAmount() { return this->currencyAmount; }
 	void update(int elapsedTime, Player & player);
@@ -77,6 +105,13 @@ public:
 	GoldCoin(Graphics &graphics, Vector2 spawnPoint);
 	~GoldCoin();
 
+	static const ItemID ID = 3001;
+
+	void use(Player &player) override {}
+
+	Items* clone() const override { return new GoldCoin(*this); }
+
+
 	void update(int elapsedTime, Player &player);
 	void draw(Graphics &graphics);
 	void drawDrops(Graphics &graphics, float x, float y);
@@ -95,6 +130,13 @@ public:
 	RedCoin();
 	RedCoin(Graphics &graphics, Vector2 spawnPoint);
 	~RedCoin();
+
+	static const ItemID ID = 3002;
+
+	void use(Player &player) override {}
+
+	Items* clone() const override { return new RedCoin(*this); }
+
 
 	void update(int elapsedTime, Player &player);
 	void draw(Graphics &graphics);
@@ -115,6 +157,12 @@ public:
 	BronzeCoin(Graphics &graphics, Vector2 spawnPoint);
 	~BronzeCoin();
 
+	static const ItemID ID = 3000;
+
+	void use(Player &player) override {}
+
+	Items* clone() const override { return new BronzeCoin(*this); }
+
 	void update(int elapsedTime, Player &player);
 	void draw(Graphics &graphics);
 	void drawDrops(Graphics &graphics, float x, float y);
@@ -133,6 +181,13 @@ public:
 	SilverGem();
 	SilverGem(Graphics &graphics, Vector2 spawnPoint);
 	~SilverGem();
+
+	static const ItemID ID = 1100;
+
+	void use(Player &player) override {}
+
+	Items* clone() const override { return new SilverGem(*this); }
+
 
 	void update(int elapsedTime, Player &player);
 	void draw(Graphics &graphics);
@@ -153,6 +208,12 @@ public:
 	Key();
 	Key(Graphics &graphics, Vector2 spawnPoint);
 	~Key();
+
+	static const ItemID ID = 1001;
+
+	void use(Player &player) override {}
+
+	Items* clone() const override { return new Key(*this); }
 
 	const inline bool isDroppedItem() { return this->wasDropped; }
 	const inline int getAmount() { return this->currencyAmount; }
