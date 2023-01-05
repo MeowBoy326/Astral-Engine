e9d4c6479fb56db1f781c5dcc34b8c6d5fc569f9
Arhum Z. Nayyar <47288337+ArhumZNayyar@users.noreply.github.com>
2023-01-04 21:24:47 -0500

    Implement drawPlayer()

--- main/src/AnimatedSprite.cpp
+++ main/src/AnimatedSprite.cpp
@@ -217,6 +217,20 @@ void AnimatedSprite::draw(Graphics &graphics, int x, int y) {
 	}
 }
 
+void AnimatedSprite::drawPlayer(Graphics& graphics, int x, int y) {
+	if (this->_visible) { // Only draw when visible
+		SDL_Rect destinationRectangle; // Temp where we draw on screen
+		destinationRectangle.x = x + this->_offsets[this->_currentAnimation].x;
+		// This will push it over to whatever we set from offset when we draw (doesnt change postion) but drawn in different position with offset
+		destinationRectangle.y = y + this->_offsets[this->_currentAnimation].y;
+		destinationRectangle.w = this->_sourceRect.w * globals::PLAYER_SCALE;
+		destinationRectangle.h = this->_sourceRect.h * globals::PLAYER_SCALE;
+
+		SDL_Rect sourceRect = this->_animation[this->_currentAnimation][this->_frameIndex]; // Pull out correct rectangle
+		graphics.blitSurface(this->_spriteSheet, &sourceRect, &destinationRectangle);
+	}
+}
+
 void AnimatedSprite::drawBoss(Graphics &graphics, int x, int y, float scaleFactor) {
 	if (this->_visible) { // Only draw when visible
 		SDL_Rect destinationRectangle; // Temp where we draw on screen
