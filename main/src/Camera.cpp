3a0e88ef2f320a87fc906144d5e99d0bb788c778
Arhum Z. Nayyar <47288337+ArhumZNayyar@users.noreply.github.com>
2023-01-06 18:12:19 -0500

    Add handling for scaling mode (higher res)

--- main/src/Camera.cpp
+++ main/src/Camera.cpp
@@ -7,35 +7,46 @@
 static float previousY;
 static SDL_Rect m_rect;
 
-void Camera::Init()
-{
-    m_rect = {0, 0, 640, 480};
-	m_rect.x = 0;
-	m_rect.y = 0;
-	m_rect.w = 640;
-	m_rect.h = 480;
-}
+void Camera::Init() {}
 
 
 SDL_Rect Camera::GetRect()
 {
-	SDL_Rect GetRect = {m_rect.x,m_rect.y,640,480};
+	SDL_Rect GetRect = {m_rect.x,m_rect.y,globals::SCREEN_WIDTH,globals::SCREEN_HEIGHT };
 	return GetRect;
 }
 
 void Camera::Update(float elapsedTime, Player &player)
 {
-	previousY = m_rect.y;
-	int newY = player.getY() - (globals::SCREEN_HEIGHT / 2);
-	if (player._currentSurface == SLOPE) {
-		// If there wasn't any significant change in Y then use the previous Y
-		// to prevent the camera from flickering vertically.
-		if (checkInRange(previousY - 3, previousY + 3, newY)) {
-			newY = previousY;
-		}
+	if (globals::SCALED_MODE) {
+
+		/* Works in all resolution cases since we just need to change the float value to integer */
+		int scale_x = 1;
+		int scale_y = 1;
+
+		/* Inverse of the scale factor to calculate player position */
+		int player_x = player.getX() / scale_x;
+		int player_y = player.getY() / scale_y;
+
+		int camera_x = player_x - (640 / 2);
+		int camera_y = player_y - (480 / 2);
+
+		m_rect.x = camera_x;
+		m_rect.y = camera_y;
 	}
-	m_rect.x = player.getX() - (globals::SCREEN_WIDTH / 2);
-	m_rect.y = newY;
+	else {
+		previousY = m_rect.y;
+		int newY = player.getY() - (globals::SCREEN_HEIGHT / 2);
+		if (player._currentSurface == SLOPE) {
+			// If there wasn't any significant change in Y then use the previous Y
+			// to prevent the camera from flickering vertically.
+			if (checkInRange(previousY - 3, previousY + 3, newY)) {
+				newY = previousY;
+			}
+		}
+		m_rect.x = player.getX() - (globals::SCREEN_WIDTH / 2);
+		m_rect.y = newY;
+	}
 }
 
 void Camera::sceneUpdate(float x, float y)
