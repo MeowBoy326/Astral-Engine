0402d6c2f6947a7bbf2c6304527ae7d6dcfffc2f
Arhum Z. Nayyar <47288337+ArhumZNayyar@users.noreply.github.com>
2023-01-09 21:13:01 -0500

    Add background scaling and resolve issues
    Parallax backgrounds of any size dimensions will scale to the window dimensions of the users choice and the positioning of the images have been corrected.

--- main/src/Parallax.cpp
+++ main/src/Parallax.cpp
@@ -1,4 +1,5 @@
 #include "../headers/Parallax.h"
+
 #include "../headers/Graphics.h"
 
 #include <SDL.h>
@@ -15,24 +16,29 @@ void Parallax::update(int elapsedTime, float dx, float dy) {
 			int imageWidth, imageHeight;
 			SDL_QueryTexture(this->bgImg, nullptr, nullptr, &imageWidth, &imageHeight);
 
+			int new_width = imageWidth * 1;
+			//if (imageWidth < globals::SCREEN_WIDTH) {
+			//	new_width = globals::SCREEN_WIDTH; //imageWidth * 2;
+			//}
+
 			if (dx > 0.0f) {
 				this->srcPosition.x += (int)((dx + 1) * this->parallaxSpeed);
 				this->srcPositionCopy.x += (int)((dx + 1) * this->parallaxSpeed);
-				if (this->srcPosition.x >= imageWidth) {
-					this->srcPosition.x = -globals::SCREEN_WIDTH; 
+				if (this->srcPosition.x >= new_width) {
+					this->srcPosition.x = -new_width;
 				}
-				if (this->srcPositionCopy.x >= imageWidth) {
-					this->srcPositionCopy.x = -globals::SCREEN_WIDTH;
+				if (this->srcPositionCopy.x >= new_width) {
+					this->srcPositionCopy.x = -new_width;
 				}
 			}
 			else {
 				this->srcPosition.x -= (int) ( (abs(dx) + 1) * this->parallaxSpeed);
 				this->srcPositionCopy.x -= (int)((abs(dx) + 1) * this->parallaxSpeed);
-				if (this->srcPosition.x <= -globals::SCREEN_WIDTH) {
-					this->srcPosition.x = globals::SCREEN_WIDTH;
+				if (this->srcPosition.x <= -new_width) { //-globals::SCREEN_WIDTH) {
+					this->srcPosition.x = new_width - 1;
 				}
-				if (this->srcPositionCopy.x <= -globals::SCREEN_WIDTH) {
-					this->srcPositionCopy.x = globals::SCREEN_WIDTH;
+				else if (this->srcPositionCopy.x <= -new_width) { //-globals::SCREEN_WIDTH) {
+					this->srcPositionCopy.x = new_width - 1;
 				}
 			}
 
@@ -45,9 +51,30 @@ void Parallax::draw(Graphics& graphics) {
 	int imageWidth, imageHeight;
 	SDL_QueryTexture(this->bgImg, nullptr, nullptr, &imageWidth, &imageHeight);
 
-	SDL_Rect destRect1 = { this->srcPosition.x, 0, globals::SCREEN_WIDTH, globals::SCREEN_HEIGHT };
-	graphics.windowBlitSurface(this->bgImg, NULL, &destRect1);
+	int new_width, new_height;
 
-	SDL_Rect destRect2 = { this->srcPositionCopy.x, 0, globals::SCREEN_WIDTH, globals::SCREEN_HEIGHT };
-	graphics.windowBlitSurface(this->bgImg, NULL, &destRect2);
+	new_height = imageHeight * 1;
+	if (imageHeight <= globals::SCREEN_HEIGHT) {
+		new_height = 480; //globals::SCREEN_HEIGHT; //imageHeight * 2;
+	}
+	//if (imageHeight <= globals::SCREEN_HEIGHT) {
+	//	new_height = imageHeight * 1; //globals::SCREEN_HEIGHT; //imageHeight * 2;
+	//	std::cout << "height = " << imageHeight << ", newHeight = " << new_height << std::endl;
+	//	if (new_height <= 400) {
+	//		new_height = imageHeight * 2;
+	//	}
+	//}
+	
+	new_width = imageWidth * 1;
+	if (imageWidth < globals::SCREEN_WIDTH) {
+		new_width = globals::SCREEN_WIDTH; //imageWidth * 2;
+	}
+
+	SDL_Rect srcRect1 = { 0, 0, new_width, new_height };
+	SDL_Rect destRect1 = { this->srcPosition.x, 0, new_width, new_height };
+	graphics.windowBlitSurface(this->bgImg, &srcRect1, &destRect1);
+
+	SDL_Rect srcRect2 = { 0, 0, new_width, new_height };
+	SDL_Rect destRect2 = { this->srcPositionCopy.x, 0, new_width, new_height };
+	graphics.windowBlitSurface(this->bgImg, &srcRect2, &destRect2);
 }
