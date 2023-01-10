4c1565bc6c3975b3df57d3d7d85d316fd66bf82d
Arhum Z. Nayyar <47288337+ArhumZNayyar@users.noreply.github.com>
2023-01-10 00:37:57 -0500

    Update Graphics.cpp

--- main/src/Graphics.cpp
+++ main/src/Graphics.cpp
@@ -47,6 +47,7 @@ SDL_Surface* Graphics::loadImage(const std::string &filePath) {
 	
 	if (this->_spriteSheets.count(filePath) == 0) {
 		this->_spriteSheets[filePath] = IMG_Load(filePath.c_str()); // will use SDL to load image, it uses a c-string
+		//printf("IMG_Load: %s\n", IMG_GetError());
 	}
 	return this->_spriteSheets[filePath]; // Regardless we will return sprite if its loaded already or needs to be
 }
